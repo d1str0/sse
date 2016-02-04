0639806be309ba4dd1b3336960f6d270a244f3f0
Brady Sullivan <brady@bsull.com>
2016-02-03 20:28:06 -0800

    Add hmac helper function.

---
+++ hmac.go
@@ -0,0 +1,15 @@
+package sse
+
+import (
+	"crypto/hmac"
+)
+
+// CheckMAC reports whether messageMAC is a valid HMAC tag for message.
+func CheckMAC(message, messageMAC, key []byte) bool {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
+// https://golang.org/pkg/crypto/hmac/
