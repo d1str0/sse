5793e951995632a4e17d021791d002a9377e0d18
Brady Sullivan <brady@bsull.com>
2016-02-10 19:36:30 -0800

    Initial pkcs7 implementation.

---
+++ pkcs7.go
@@ -0,0 +1,23 @@
+package sse
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func Pad(src []byte) []byte {
+	padding := aes.BlockSize - len(src)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+func Unpad(src []byte) ([]byte, error) {
+	length := len(src)
+	unpadding := int(src[length-1])
+
+	if unpadding > length {
+		return nil, errors.New("unpad error. This could happen when incorrect encryption key is used")
+	}
+
+	return src[:(length - unpadding)], nil
+}
