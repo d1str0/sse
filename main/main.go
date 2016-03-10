e67ca24a645b3cbdb42b47ae560f254d6fc35e29
Brady Sullivan <brady@bsull.com>
2016-03-09 23:13:13 -0800

    Update client to add keywords and properly search on keywords.

--- main/main.go
+++ main/main.go
@@ -42,7 +42,7 @@ func main() {
 	var db sse.DBConn
 	db, err := sse.BoltDBOpen()
 	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
+		fmt.Printf("Error creating BoltDB database: %v", err)
 		os.Exit(1)
 	}
 	defer db.Close()
@@ -51,53 +51,74 @@ func main() {
 
 	err = db.Init()
 	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
+		fmt.Printf("Error on db.Init(): %v", err)
 		os.Exit(1)
 	}
-	db.Put(sse.DOCUMENTS, h1, c1)
+	err = db.Put(sse.DOCUMENTS, h1, c1)
 	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
+		fmt.Printf("Error putting document1: %v", err)
 		os.Exit(1)
 	}
 	db.Put(sse.DOCUMENTS, h2, c2)
-	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
-		os.Exit(1)
-	}
 	db.Put(sse.DOCUMENTS, h3, c3)
-	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
-		os.Exit(1)
-	}
 
 	cc1, err := db.Get(sse.DOCUMENTS, h1)
 	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
+		fmt.Printf("Error getting h1: %v", err)
 		os.Exit(1)
 	}
 	bb1 := sse.CheckMAC(cc1, h1, key)
 
-	cc2, err := db.Get(sse.DOCUMENTS, h2)
-	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
-		os.Exit(1)
-	}
+	cc2, _ := db.Get(sse.DOCUMENTS, h2)
 	bb2 := sse.CheckMAC(cc2, h2, key)
 
-	cc3, err := db.Get(sse.DOCUMENTS, h3)
-	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
-		os.Exit(1)
-	}
+	cc3, _ := db.Get(sse.DOCUMENTS, h3)
 	bb3 := sse.CheckMAC(cc3, h3, key)
 	fmt.Printf("CheckMAC (after DB):\n\t1: %t\n\t2: %t\n\t3: %t\n\n", bb1, bb2, bb3)
 
 	err = db.Delete(sse.DOCUMENTS, h1)
-	err = db.Delete(sse.DOCUMENTS, h2)
-	err = db.Delete(sse.DOCUMENTS, h3)
 	if err != nil {
-		fmt.Printf("Error creating BoldDB database: %v", err)
+		fmt.Printf("Error deleting h1: %v", err)
 		os.Exit(1)
+	}
+	db.Delete(sse.DOCUMENTS, h2)
+	db.Delete(sse.DOCUMENTS, h3)
+
+	c := sse.NewClient(db)
+	c.SetKey("hunter2", "farts", 4096)
+	err = c.Put("test1", test1)
+	if err != nil {
+		fmt.Printf("Error putting test1: %v", err)
+		os.Exit(1)
+	}
+	c.Put("test2", test2)
+	c.Put("test3", test3)
+
+	doc1, err := c.Get("test1")
+	if err != nil {
+		fmt.Printf("Error getting test1: %v", err)
+		os.Exit(1)
+	}
+	doc2, _ := c.Get("test2")
+	doc3, _ := c.Get("test3")
+
+	fmt.Printf("doc1: %s\ndoc2: %s\ndoc3: %s\n", doc1, doc2, doc3)
+
+	err = c.AddDocToKeyword("farts", "test1")
+	if err != nil {
+		fmt.Printf("Error adding farts to test1: %v", err)
+		os.Exit(1)
+	}
+	c.AddDocToKeyword("farts", "test2")
+	c.AddDocToKeyword("farts", "test3")
+
+	ids, err := c.Search("farts")
+	if err != nil {
+		fmt.Printf("Error searching for farts: %v", err)
+		os.Exit(1)
+	}
+	for _, s := range ids {
+		fmt.Println(s)
 	}
 
 	// Grab the current stats and diff them.
