03baed6ecafdbf474a924c5faf8f021715433447
Brady Sullivan <brady@bsull.com>
2016-03-13 22:27:44 -0700

    Update main to crawl through all files in a directory.

--- main/main.go
+++ main/main.go
@@ -1,23 +1,90 @@
 package main
 
 import (
+	// "encoding/json"
+	"flag"
 	"fmt"
 	//	"github.com/d1str0/sse"
-	"io/ioutil"
-	"net/mail"
+	//	"io/ioutil"
+	//	"net/mail"
 	"os"
 	//	"strings"
 )
 
-const mailFile = "1.txt"
+var mailDir string // Directory to load
+
+var fileCount int
+var dirCount int
+
+type Identity struct {
+	password string
+	salt     string
+	iter     int
+}
+
+var id = Identity{
+	password: "hunter2",
+	salt:     "So Salty",
+	iter:     4096,
+}
 
 func main() {
-	f, err := os.Open(mailFile)
+
+	flag.StringVar(&mailDir, "mail-dir", "", "directory to load mail archives from")
+	flag.Parse()
+
+	ReadAllFiles(mailDir)
+
+	fmt.Printf("%d total files in %d different directories!\n", fileCount, dirCount)
+}
+
+func ReadAllFiles(filename string) {
+	f, err := os.Open(filename)
 	if err != nil {
-		fmt.Printf("Error opening file: %#v\n", err)
+		fmt.Printf("Error opening file %s: %s\n", filename, err.Error())
 		os.Exit(1)
 	}
 
+	stat, err := f.Stat()
+	if err != nil {
+		fmt.Printf("Error opening file stat %s: %s\n", filename, err.Error())
+		os.Exit(1)
+	}
+
+	if stat.IsDir() {
+		files, err := f.Readdir(0)
+		if err != nil {
+			fmt.Printf("Error reading directory %s: %s\n", stat.Name(), err.Error())
+			os.Exit(1)
+		}
+		//fmt.Printf("%s/\n", stat.Name())
+
+		err = f.Close()
+		if err != nil {
+			fmt.Printf("Error closing directory %s: %s\n", stat.Name(), err.Error())
+			os.Exit(1)
+		}
+
+		for _, file := range files {
+
+			ReadAllFiles(fmt.Sprintf("%s/%s", filename, file.Name()))
+		}
+
+		dirCount++
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Printf("Error closing file %s: %s\n", stat.Name(), err.Error())
+		os.Exit(1)
+	}
+
+	//fmt.Printf("%s\n", stat.Name())
+
+	fileCount++
+}
+
+/*
 	m, err := mail.ReadMessage(f)
 	if err != nil {
 		fmt.Printf("Error reading mail message: %#v\n", err)
@@ -38,3 +105,4 @@ func main() {
 	fmt.Printf("%s", body)
 
 }
+*/
